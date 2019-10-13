2bbbe7193a437f37e3d61bc21ebdf5c56c21123f
Sandeep Cherukuri <sandeep.cherukuri@sapientrazorfish.com>
2019-10-13 10:37:18 -0500

    updated created restaurant data

--- model/music.js
+++ model/music.js
@@ -1,7 +1,7 @@
 var mongoose    =   require("mongoose");
 
 var mongoUri = process.env.MONGODB_URI || 'mongodb://chsandeep:Sandeep@511@ds017195.mlab.com:17195/heroku_nr1vzxjj' ;
-mongoose.connect(mongoUri); 
+mongoose.connect(mongoUri);
 
 //mongoose.connect('mongodb://localhost:27017/test');
 
@@ -23,9 +23,9 @@ var Album = new Schema({
 
 
 // Music Model
-var Music =new Schema({  
-    title: { type: String, required : "true"},  
-    title_image_url: { type: String, required : "true" },  
+var Music =new Schema({
+    title: { type: String, required : "true"},
+    title_image_url: { type: String, required : "true" },
     album: [Album],
     modified: { type: Date, default: Date.now }
 });
